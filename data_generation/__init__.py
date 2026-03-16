8569a3d636ed1fffd6bbd72e0980250bd5f94f51
NayukiChiba <MekoNayuki@outlook.com>
2026-03-16 20:48:32 +0800

    refactor(regression): 重构回归算法的数据生成方法，统一管理

--- data_generation/__init__.py
+++ data_generation/__init__.py
@@ -1,21 +1,28 @@
 from .classification import (
     logistic_regression_data,
-    decision_tree_data,
+    decision_tree_classification_data,
     svc_data,
     naive_bayes_data,
     knn_data,
     random_forest_data,
 )
+
 from .regression import (
     linear_regression_data,
+    svr_data,
+    decision_tree_regression_data,
+    regularization_data,
 )
 
 __all__ = [
     "logistic_regression_data",
-    "decision_tree_data",
+    "decision_tree_classification_data",
     "svc_data",
     "naive_bayes_data",
     "knn_data",
     "random_forest_data",
     "linear_regression_data",
+    "svr_data",
+    "decision_tree_regression_data",
+    "regularization_data",
 ]
